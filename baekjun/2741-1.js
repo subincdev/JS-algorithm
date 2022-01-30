5e597d73174aad47cc116bd594571882befab872
subincdev <subinc.dev@gmail.com>
2022-01-30 21:34:00 +0900

    알고리즘 문제풀이

---
+++ baekjun/2741-1.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('stdin.txt').toString();
+
+console.log(input);
+
+let inputsecond = Number(require('fs').readFileSync('stdin.txt').toString());
+
+console.log(inputsecond);
