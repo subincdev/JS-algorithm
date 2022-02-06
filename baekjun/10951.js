27e8d6e5966026ccaff20de487947d157cfc1153
subincdev <subinc.dev@gmail.com>
2022-02-06 11:48:00 +0900

    알고리즘 문제풀이

---
+++ baekjun/10951.js
@@ -0,0 +1,15 @@
+let input = require('fs').readFileSync('stdin.txt').toString();
+let n = parseInt(input);
+let i = 0;
+let m = 0;
+while (true) {
+    let m = parseInt((n%10)*10+(n/10+n%10)%10)
+    i++;
+    if (parseInt(input) == m) {
+        break;
+    }
+    else {
+        n=m;
+    }
+}
+console.log(i);
