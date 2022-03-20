dfca7c9d73e0b3aab6cceefc52a7bb898e33288a
subincdev <subinc.dev@gmail.com>
2022-03-20 11:45:00 +0900

    알고리즘 문제풀이

---
+++ baekjun/10872.js
@@ -0,0 +1,13 @@
+const input= require('fs').readFileSync('stdin.txt').toString().trim();
+const N = +input;
+let count=0;
+let answer=[];
+//num: 원반의 개수
+//from:출발지 기둥 번호
+//to: 목적지 기둥 번호
+//other: 나머지 기둥 번호
+function factorial(n){
+
+}
+
+console.log(factorial(input));
